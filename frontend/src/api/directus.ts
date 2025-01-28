d2f13b83521f51051886468da34942db49c448a3
Patrick (The Website Guy) <patrick@thewebsiteguy.uk>
2025-01-28 12:39:45 +0000

    wip

--- frontend/src/api/directus.ts
+++ frontend/src/api/directus.ts
@@ -18,6 +18,10 @@ type Page = {
   featured_image: string;
 }
 
+type BusinessInfo = {
+  contact_number: string;
+}
+
 type Treatments = {
   name: string;
   description: string;
@@ -25,6 +29,17 @@ type Treatments = {
   cost: string;
   booking_link: string;
   disclaimer: string;
+}
+
+type TreatmentPage = {
+  featured_image: string;
+  title: string;
+  headline: string;
+  excerpt: Author;
+  content: string;
+  published_date: string
+  slug: string;
+  treatments: Treatments;
 }
 
 type TreatmentPages = {
@@ -45,6 +60,8 @@ type Schema = {
   global: Global;
   pages: Page[];
   treatments: Treatments[];
+  business_info: BusinessInfo;
+  treatment_page: TreatmentPage;
 }
 
 const directus = createDirectus<Schema>('http://localhost:8055').with(rest());
