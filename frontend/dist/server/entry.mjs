c3c267c19a73adce0578ed39a2d6573d5bc54cd6
Patrick McGonigle <patrick@thewebsiteguy.uk>
2025-02-02 13:39:09 +0000

    wip

--- frontend/dist/server/entry.mjs
+++ frontend/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_DhBU0s4G.mjs';
-import { manifest } from './manifest_CITdGvAv.mjs';
+import { manifest } from './manifest_BsfqJCpO.mjs';
 
 const serverIslandMap = new Map();;
 
