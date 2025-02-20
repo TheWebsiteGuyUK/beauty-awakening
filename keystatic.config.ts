fa3b8f17a92f1bece148b1f6a6c3d11be66a0743
Patrick McGonigle <patrick@thewebsiteguy.uk>
2025-02-20 15:12:10 +0000

    initital commit

---
+++ keystatic.config.ts
@@ -0,0 +1,30 @@
+import { config, fields, collection } from '@keystatic/core';
+
+export default config({
+   storage: {
+    kind: 'cloud',
+  },
+  cloud: {
+    project: 'the-website-guy-uk/beauty-awakening',
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        content: fields.markdoc({
+          label: 'Content',
+          options: {
+            image: {
+              directory: 'src/assets/images/posts',
+              publicPath: '../../assets/images/posts/',
+            },
+          },
+        }),
+      },
+    }),
+  },
+});
