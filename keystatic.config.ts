dc3ea0a92b4f252e50400f4908fe127de16b525a
Patrick (The Website Guy) <patrick@thewebsiteguy.uk>
2025-05-11 10:29:58 +0100

    Refactor configuration and styling: switch storage to cloud, adjust spacing in index page, enhance treatment page layout, and reorder CSS imports

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -4,11 +4,11 @@ import { home, branding, seo, company, about } from "@cms/singletons";
 
 export default config({
   storage: {
-    kind: 'local',
+    kind: 'cloud',
   },
-  // cloud: {
-  //   project: 'beauty-awakening/beauty-awakening',
-  // },
+  cloud: {
+    project: 'beauty-awakening/beauty-awakening',
+  },
   ui: {
     brand: { name: "Beauty Awakening CMS" },
     navigation: {
